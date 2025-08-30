bf3837906065db585b9e4140fd130d232cc3cefa
Brandan Brajkovich <brandan.brajkovich@hotmail.com>
2025-08-30 08:33:02 -0500

    ActiveAdmin: register Customer with CRUD and image upload

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -47,3 +47,36 @@ ActiveAdmin.register Customer do
     active_admin_comments
   end
 end
+# frozen_string_literal: true
+
+ActiveAdmin.register Customer do
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
+
+  index do
+    selectable_column
+    id_column
+    column :full_name
+    column :phone_number
+    column :email_address
+    column :notes
+    if Customer.reflect_on_attachment(:image)
+      column :image do |customer|
+        if customer.image.attached?
+          image_tag url_for(customer.image), size: "50x50"
+        end
+      end
+    end
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+end
